3016355f6a9b24705e3a1d00623b827884ad6bd8
lucas <lucaspasquotto@outlook.com>
2023-09-26 08:09:30 -0300

    feat: Car and Vehicle by interface lesson

---
+++ src/lesson3/interfaceExample/Vehicle.java
@@ -0,0 +1,7 @@
+package lesson3.interfaceExample;
+
+public interface Vehicle {
+    public abstract String getType();
+    public abstract String getSpeed();
+    public abstract String getColor();
+}
