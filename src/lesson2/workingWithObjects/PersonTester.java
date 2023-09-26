463447e9f9a1734f707e3265b5acdf5edb25c7f2
lucas <lucaspasquotto@outlook.com>
2023-09-26 08:07:00 -0300

    feat: Person and Persson Testes by working with objects lesson

---
+++ src/lesson2/workingWithObjects/PersonTester.java
@@ -0,0 +1,12 @@
+package lesson2.workingWithObjects;
+
+public class PersonTester {
+
+    public static void main(String[] args) {
+        Person bob = new Person("Bob", "Joseph");
+        Person mike = new Person("Mike", "Fikel");
+
+        System.out.println(bob);
+        System.out.println(mike);
+    }
+}
