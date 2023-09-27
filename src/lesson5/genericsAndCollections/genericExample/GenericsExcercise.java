60d6f61564241abee9c7c3b087b83940c55a23b3
lucas <lucaspasquotto@outlook.com>
2023-09-27 09:42:38 -0300

    feat: Generics Excercises

---
+++ src/lesson5/genericsAndCollections/genericExample/GenericsExcercise.java
@@ -0,0 +1,28 @@
+package lesson5.genericsAndCollections.genericExample;
+
+import java.util.ArrayList;
+
+public class GenericsExcercise {
+    public static void main(String[] args) {
+        ArrayList<Object> variables = new ArrayList<Object>();
+
+        Double doubleNumber = 1.5;
+        String name = "Sally";
+        Integer intNumber = 1;
+        Character letter = 'a';
+
+        variables.add(doubleNumber);
+        variables.add(name);
+        variables.add(intNumber);
+        variables.add(letter);
+
+        for (Object variable : variables) {
+            GenericsExcercise.displayClassName(variable);
+        }
+    }
+
+    // Java type ensure
+    public static <T> void displayClassName(T variable) {
+        System.out.println(variable.getClass().getName());
+    }
+}
