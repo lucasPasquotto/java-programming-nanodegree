8e1d8a4972216e3f5d08e678ff6f9eb14e688dec
lucas <lucaspasquotto@outlook.com>
2023-09-27 09:42:18 -0300

    feat: Comment on RegexTester

--- src/lesson4/regExExample/RegExTester.java
+++ src/lesson4/regExExample/RegExTester.java
@@ -4,6 +4,10 @@ import java.util.regex.Pattern;
 
 public class RegExTester {
 
+    /**
+     * https://docs.oracle.com/javase/7/docs/api/java/util/regex/Pattern.html
+     * https://regexr.com/
+     */
     public static void main(String[] args) {
         String emailRegex = "^(.+)@(.+).(.+)$";
         Pattern pattern = Pattern.compile(emailRegex);
