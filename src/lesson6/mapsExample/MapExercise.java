e40ea4032954311e05f4b18723c26c67449596db
lucas <lucaspasquotto@outlook.com>
2023-10-04 09:57:19 -0300

    feat: Maps Exercises

---
+++ src/lesson6/mapsExample/MapExercise.java
@@ -0,0 +1,54 @@
+package lesson6.mapsExample;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Notes: In computer science, the efficiency of an algorithm is often expressed using Big O notation.
+ * Maps allow us to find an element in O(1) time complexity,
+ * which is more efficient than the O(n) time complexity required for a linear search, such as in Arrays.
+ * Use Maps to retrieve an element from a list in constant time
+ *
+ * Use Set to avoid duplicates in the list
+ * Use Queue to process the elements in the order they arrive
+ */
+public class MapExercise {
+
+    public static void main(String[] args) {
+        Map<String, Person> mapOpPeople = new HashMap<String, Person>();
+
+        Person mike = new Person("Mike", "mike@email.com");
+        Person shaun = new Person("Shaun", "shaun@email.com");
+        Person sally = new Person("Sally", "sally@email.com");
+        Person cesar = new Person("Cesar", "cesar@email.com");
+
+        ArrayList<Person> people = new ArrayList<Person>();
+        people.add(mike);
+        people.add(shaun);
+        people.add(sally);
+        people.add(cesar);
+
+        for (Person person : people) {
+            MapExercise.addToMap(mapOpPeople, person);
+        }
+
+        for (String email : mapOpPeople.keySet()) {
+            System.out.println(email);
+        }
+
+        for (Person person : mapOpPeople.values()) {
+            System.out.println(person);
+        }
+
+        System.out.println("Get Mike: " + mapOpPeople.get("mike@email.com"));
+        System.out.println("Get Jeff: " + mapOpPeople.get("jeff@email.com"));
+        System.out.println("Contains Mike: " + mapOpPeople.containsKey("mike@email.com"));
+        System.out.println("Contains Jeff: " + mapOpPeople.containsKey("jeff@email.com"));
+
+    }
+
+    private static void addToMap(Map<String, Person> map, Person person) {
+        map.put(person.getEmail(), person);
+    }
+}
---
+++ src/lesson6/mapsExample/Person.java
@@ -0,0 +1,32 @@
+package lesson6.mapsExample;
+
+public class Person {
+    String name;
+    String email;
+
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person name: " + name + " email: " + email;
+    }
+}
